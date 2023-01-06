bf538ed8d6688a4ca7eeca2215aecc9576899258
Kate Chalmers <74201925+kate-chalmers@users.noreply.github.com>
2023-01-06 13:34:15 +0100

    debugging unknown error

--- test.R
+++ test.R
@@ -10,9 +10,7 @@ remDr$open()
 remDr$navigate("https://phptravels.com/demo")
 remDr$navigate("https://www.eccb-centralbank.org/statistics/gdp-datas/comparative-report/1")
 
-# remDr$findElement(using = "id", value = "categories-ids-ngdp_mc")$clickElement()
-
-
+remDr$findElement(using = "id", value = "categories-ids-ngdp_mc")$clickElement()
 
 html <- remDr$getPageSource()
 

87baadd62a2848b4da5ea76c83b9944694a77cae
Kate Chalmers <74201925+kate-chalmers@users.noreply.github.com>
2023-01-06 14:00:04 +0100

    try with different link

--- test.R
+++ test.R
@@ -5,26 +5,19 @@ Sys.sleep(5)
 system("docker run -d --shm-size='2g' -p 4445:4444 selenium/standalone-chrome", wait=TRUE)
 Sys.sleep(5)
 
+# rD <- rsDriver(browser = "firefox", chromever = NULL, port = netstat::free_port())
+# remDr <- rD[["client"]]
+
 remDr <- remoteDriver("localhost", 4445L, "chrome")
 remDr$open()
-remDr$navigate("https://phptravels.com/demo")
-remDr$navigate("https://www.eccb-centralbank.org/statistics/gdp-datas/comparative-report/1")
 
-remDr$findElement(using = "id", value = "categories-ids-ngdp_mc")$clickElement()
+url5 <- "https://www.eccb-centralbank.org/statistics/debt-datas/comparative-report/2"
+
+remDr$navigate(url5)
+
+remDr$findElement(using = "id", value = "categories-ids-dgdpcg")$clickElement()
 
 html <- remDr$getPageSource()
 
-# writeChar(html[[1]], "result.html")
+writeChar(html[[1]], "result.html")
 
-# url <- "https://www.eccb-centralbank.org/statistics/gdp-datas/comparative-report/1"
-# 
-# remDr$navigate(url)
-# 
-# remDr$findElement(using = "id", value = "categories-ids-ngdp_mc")$clickElement()
-# 
-# remDr$findElement(using = "id", value = "start-date")$sendKeysToElement(list("2019-12-31"))
-# remDr$findElement(using = "id", value = "end-date")$sendKeysToElement(list(end_date))
-# 
-# remDr$findElement(using = "id", value = "statistics-select-all-countries")$clickElement()
-# 
-# remDr$findElements("css", ".form-buttom")[[1]]$clickElement()

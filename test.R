6cf2390a5250d8b98f6ed115d36ce3fbe1a91010
Kate Chalmers <74201925+kate-chalmers@users.noreply.github.com>
2023-01-06 13:41:14 +0100

    increasing shm size

--- test.R
+++ test.R
@@ -2,7 +2,7 @@ library(RSelenium)
 
 system("docker pull selenium/standalone-chrome", wait=TRUE)
 Sys.sleep(5)
-system("docker run -d -p 4445:4444 selenium/standalone-chrome", wait=TRUE)
+system("docker run -d --shm-size='2g' -p 4445:4444 selenium/standalone-chrome", wait=TRUE)
 Sys.sleep(5)
 
 remDr <- remoteDriver("localhost", 4445L, "chrome")

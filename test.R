302a76c5b5ba12e45469aa4f2636eac7bbd5ba40
Kate Chalmers <74201925+kate-chalmers@users.noreply.github.com>
2023-01-06 12:13:58 +0100

    further testing

--- test.R
+++ test.R
@@ -8,6 +8,10 @@ Sys.sleep(5)
 remDr <- remoteDriver("localhost", 4445L, "chrome")
 remDr$open()
 remDr$navigate("https://phptravels.com/demo")
+remDr$navigate("https://www.eccb-centralbank.org/statistics/gdp-datas/comparative-report/1")
+
+remDr$findElement(using = "id", value = "categories-ids-ngdp_mc")$clickElement()
+
 html <- remDr$getPageSource()
 
 writeChar(html[[1]], "result.html")
